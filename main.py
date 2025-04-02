e39bec9ab3bb1d58c9dcd7cb8fc7c3bb50d5d4f5
jjackson112 <jjackson11211@gmail.com>
2025-04-02 19:51:39 -0400

    Villain list

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ websites.pop(2)
 websites.insert(2, "www.yahoo.com")
 websites.append("refinery29.com")
 print(websites)
+
+villains = [
+    "Shan Yu", "Ursula", "Gaston", "Jafar", "Maleficent", "Scar",
+    "Mother Gothel", "Cruella", "Lady Tremaine", "Queen of Hearts", "Hades",
+    "Dr. Facilier", "Ratigan", "Hans", "Yzma", "Captain Hook", "Frollo",
+    "Syndrome", "Screenslaver", "Tamatoa"
+]
+
+first_four = villains[0:4]
+print(first_four)
+
+last_four = villains[16:20]
+print(last_four)
+
+middle_four = villains[10:13]
+print(middle_four)
+
+only_one = villains[9]
+print(only_one)
